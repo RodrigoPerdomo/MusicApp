413f0e218bdf1a11f51518019b8a90d441757709
RodrigoPerdomo <rodriper1994@gmail.com>
2021-02-24 18:42:56 -0300

    First Commit

---
+++ MusicApp.py
@@ -0,0 +1,36 @@
+# Aplicacion por consola que trata sobre Musica, en la que te indica la cantidad de canciones que el usuario ingresa y cuanto tiempo dura el Disco en total.
+
+DuracionDisco=0.00
+CantidadCanciones=0
+
+print("***************************")
+print("Ingrese el nombre del Disco")
+print("***************************")
+nombreDisco=input()
+print("**************************************")
+print("Desea agregar una cancion al disco S/N")
+print("**************************************")
+respuesta=input()
+while respuesta == "s" or respuesta== "S":
+    print("****************************")
+    print("Ingrese nombre de la cancion")
+    print("****************************")
+    nombreCancion=input()
+    CantidadCanciones+=1
+    if nombreCancion !="":
+        print("******************************")
+        print("Ingrese duracion de la cancion")
+        print("******************************")
+        DuracionCancion=float(input())
+        if DuracionCancion >0:
+            DuracionDisco+=DuracionCancion
+    print("**************************************")
+    print("Desea agregar una cancion al disco S/N")
+    print("**************************************")
+    respuesta=input()
+else:
+    print("El nombre del Disco es: ",nombreDisco)
+    print("La cantidad de las canciones es de: ", CantidadCanciones)
+    print("El tiempo total del Disco es de: ",DuracionDisco)
+    print("Gracias por utilizar mi App <3 !!")
+    
